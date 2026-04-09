da17e522a5ba994744c2c85a789ad43aa78c69b2
Khushi Yadav <yadavkhushi0103@gmail.com>
2026-04-09 08:16:06 +0530

    add task functionaly done and completed task functionality is done

---
+++ todo/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import get_object_or_404, render , redirect
+from .models import Task
+
+
+# Create your views here.
+
+def addTask(request):
+    task = request.POST['task']
+    Task.objects.create(task = task)
+    return redirect('home')
+
+def mark_as_done(request , pk):
+    task = get_object_or_404(Task , pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+    
